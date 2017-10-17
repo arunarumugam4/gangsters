30af0eb9eaddd6119c8ba2cc1ca216c02ff5d6d0
arun <coolarun477@gmail.com>
2017-10-16 18:53:39 -0700

    database local to cloud

--- config/database.js
+++ config/database.js
@@ -1,5 +1,6 @@
 // DATABASE URL
-const dbUrl = 'mongodb://localhost:27017/gangster';
+/*const dbUrl = 'mongodb://localhost:27017/gangster';*/
+const dbUrl = "mongodb://cool:cool@ds121945.mlab.com:21945/gangsters"
 const dbName = 'gangster';
 
 // EXPORT

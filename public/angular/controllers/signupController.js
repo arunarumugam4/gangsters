25be8c470dce19a33ab1d7f35393b331ddbe282f
Arun <coolarun477@gmail.com>
2018-08-14 19:25:15 +0530

    SIGNUP EVENT ADDED

--- public/angular/controllers/signupController.js
+++ public/angular/controllers/signupController.js
@@ -16,7 +16,8 @@ app.controller('signupController', ['$http','$location', function($http,$locatio
         
         // LODER
          self.load = true;
-
+          
+	  amplitude.getInstance().logEvent('SIGNUP_EVENT);
 
         // CHECK IF PASSWORD MATCH OR NOT
         if(self.userData.password !== self.userData.confirmPassword){
